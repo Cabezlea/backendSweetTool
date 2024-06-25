88f090bd862bdb0fa12d74c0fc57c09532e4891e
Cabezlea <Leocabezas@icloud.com>
2024-06-25 11:28:50 -0500

    Connected front with back-end, set up database connection and also established the connection via URL and to make sure the domain only works with this backend server and no one else can add a server to fetch data from (security reasons)

--- backendSweetTool/settings.py
+++ backendSweetTool/settings.py
@@ -37,11 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',  # Make sure it's 'rest_framework', not 'restframework'
-    'toolsweetapp',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,3 +129,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5174',  # This is your Vue.js frontend address
+]
--- toolsweetapp/views.py
+++ toolsweetapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+#Create views here
+
+class TestAPIView(APIView):
+    def get(self, request):
+        data = {"message": "Hello from Django"}
+        return Response(data)
